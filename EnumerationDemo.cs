941def6d27777fe03a1b3fa20f6eed013a7f93ef
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-29 10:01:57 +0200

    init

---
+++ EnumerationDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _08.EnumerationsSetOfValues
+{
+    class EnumerationDemo
+    {
+
+        public enum Day  //this is enumeration
+        {
+           Monday, 
+           Tuesday, 
+           Wednesday, 
+           Thursday, 
+           Friday,
+           Saturday, 
+           Sunday
+        };
+        
+        static void Main(string[] args)
+        {
+
+            Day day = Day.Monday; //We make a variable of rthe type enum Day
+            Console.WriteLine(day);
+
+            Day nextDay = day + 1;
+            Console.WriteLine(nextDay);
+
+            Day thirdDay = nextDay + 1;
+            Console.WriteLine(thirdDay);
+            
+        }
+
+        
+        //ebnumerations are date type, predefinded, example, days of week, colors, 
+        // we define enum with enum keyword and name of the enum here in our example Color
+        // Complilation error! -We can  use only preddifined values !!!! here -only Red,Green, Blue , Black
+            //enum is data type, predifened data type;
+        
+
+    }
+}
+        
+    
+
